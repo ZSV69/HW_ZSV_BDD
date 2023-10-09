20dd210d71123d0651726b03dc16dfb7f96c146d
Zvyagintsev Sergey <zsv069@gmail.com>
2023-10-10 02:25:35 +0300

    Failed Test

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,46 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.web.data.DataHelper.*;
+
+class MoneyTransferTest {
+
+  DashboardPage dashboardPage;
+  CardInfo firstCardInfo;
+  CardInfo secondCardInfo;
+
+  @BeforeEach
+  void setup() {
+    open("http://localhost:9999");
+    var loginPage = new LoginPage();
+    var authInfo = DataHelper.getAuthInfo();
+    var verificationPage = loginPage.validLogin(authInfo);
+    var verificationCode = getVerificationCodeFor();
+    verificationPage.validVerify(verificationCode);
+    firstCardInfo = getFirstCardInfo();
+    secondCardInfo = getSecondCardInfo();
+  }
+
+  @Test
+  void shouldTransferFromFirsToSecond() {
+    var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+    var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+    var amount = generateValidAmount(firstCardBalance);
+    var expectedBalanceFirstCard = firstCardBalance - amount;
+    var expectedBalanceSecondCard = secondCardBalance + amount;
+    var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+    dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+    var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+    var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+    assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+    assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+  }
+}
+
